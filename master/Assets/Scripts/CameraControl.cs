5f64f4525ac88e264efe1102d04beb91d090ba5c
Frictionpowered <denes@nagymathe.com>
2021-10-02 00:23:21 +0100

    navigation update

--- master/Assets/Scripts/CameraControl.cs
+++ master/Assets/Scripts/CameraControl.cs
@@ -4,8 +4,23 @@ using UnityEngine;
 
 public class CameraControl : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+	public Camera cam;
+	public float CamDistance = 1.0f;
+	public float CamHeight = 1.0f;
+	public AnimationCurve CamZ;
+	public AnimationCurve CamY;
+
+	public float sensitivityX = 10.0f;
+	public float sensitivityY = 10.0f;
+
+	public float pitchMin = -70;
+	public float pitchMax = 89;
+
+	[Range(-1, 1)]
+	public float pitch = 0.0f;
+
+	// Start is called before the first frame update
+	void Start()
     {
         
     }
@@ -13,6 +28,35 @@ public class CameraControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
-    }
+		if (Cursor.lockState == CursorLockMode.Locked)
+		{
+			float x = Input.GetAxisRaw("Mouse X");
+			float y = Input.GetAxisRaw("Mouse Y");
+
+			Vector3 euler = transform.rotation.eulerAngles;
+			if (euler.x > 90.0f) euler.x -= 360.0f;
+			euler.y += x * sensitivityX;
+			euler.x -= y * sensitivityY;
+			euler.y = (euler.y + 360.0f) % 360.0f;
+			euler.x = Mathf.Clamp(euler.x, pitchMin, pitchMax);
+			transform.rotation = Quaternion.Euler(euler);
+
+			pitch = euler.x / 90.0f;
+			Vector3 camPos = cam.transform.localPosition;
+			camPos.z = CamZ.Evaluate(pitch) * CamDistance;
+			camPos.y = CamY.Evaluate(pitch) * CamHeight;
+			cam.transform.localPosition = camPos;
+
+			if (Input.GetKeyDown(KeyCode.Escape))
+			{
+				Cursor.lockState = CursorLockMode.None;
+			}
+		} else
+		{
+			if (Input.GetMouseButton(0))
+			{
+				Cursor.lockState = CursorLockMode.Locked;
+			}
+		}
+	}
 }
--- master/Assets/Scripts/ShipMovement.cs
+++ master/Assets/Scripts/ShipMovement.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class ShipMovement : MonoBehaviour
 {
+	public Transform planetRoot;
+	public float radius;
+
+	public Vector3 velocity;
+
+	public Vector3 angVel;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +20,23 @@ public class ShipMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
-    }
+		if (Input.GetKeyDown(KeyCode.W))
+			velocity.z += 5.0f;
+		if (Input.GetKeyDown(KeyCode.S))
+			velocity.z -= 5.0f;
+		if (Input.GetKeyDown(KeyCode.A))
+			velocity.y -= 5.0f;
+		if (Input.GetKeyDown(KeyCode.D))
+			velocity.y += 5.0f;
+
+		angVel.x = -velocity.z / radius;
+		angVel.z = velocity.x / radius;
+		angVel.y = -velocity.y;
+
+		if (angVel.magnitude > 0.001f)
+		{
+			Quaternion turn = Quaternion.AngleAxis(angVel.magnitude * Time.deltaTime, angVel.normalized);
+			planetRoot.rotation = turn * planetRoot.rotation;
+		}
+	}
 }
