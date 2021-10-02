29eebf8634e2b33241b825aa9502942265482b92
aik6980 <aik6980>
2021-10-02 17:09:29 +0100

    constellation canvas v1

--- master/Assets/Scripts/Gameplay/Starpicking.cs
+++ master/Assets/Scripts/Gameplay/Starpicking.cs
@@ -4,40 +4,109 @@ using System.Collections.Generic;
 
 [ExecuteInEditMode]
 public class Starpicking : MonoBehaviour {
-  ParticleSystem ps;
+    ParticleSystem ps;
 
-  // these lists are used to contain the particles which match
-  // the trigger conditions each frame.
-  List<ParticleSystem.Particle> enter = new List<ParticleSystem.Particle>();
-  List<ParticleSystem.Particle> exit = new List<ParticleSystem.Particle>();
+    // these lists are used to contain the particles which match
+    // the trigger conditions each frame.
+    List<ParticleSystem.Particle> enter = new List<ParticleSystem.Particle>();
+    List<ParticleSystem.Particle> exit = new List<ParticleSystem.Particle>();
 
-  void OnEnable() {
-    ps = GetComponent<ParticleSystem>();
-  }
+    List<ParticleSystem.Particle> inside = new List<ParticleSystem.Particle>();
 
-  void OnParticleTrigger() {
-    // get the particles which matched the trigger conditions this frame
-    int numEnter = ps.GetTriggerParticles(ParticleSystemTriggerEventType.Enter, enter);
-    int numExit = ps.GetTriggerParticles(ParticleSystemTriggerEventType.Exit, exit);
 
-    // iterate through the particles which entered the trigger and make them red
-    for (int i = 0; i < numEnter; i++) {
-      ParticleSystem.Particle p = enter[i];
-      p.startColor = new Color32(255, 0, 0, 255);
+    // Constellation struct
+    Constellation constellation;
+    LineRenderer tmp_line;
+    public Material line_material;
 
-      // Debug.Log("test enter");
-      enter[i] = p;
+    private void Start()
+    {
+        constellation = new Constellation();
     }
 
-    // iterate through the particles which exited the trigger and make them green
-    for (int i = 0; i < numExit; i++) {
-      ParticleSystem.Particle p = exit[i];
-      p.startColor = new Color32(255, 255, 255, 255);
-      exit[i] = p;
+
+    void OnEnable() 
+    {
+        ps = GetComponent<ParticleSystem>();
     }
 
-    // re-assign the modified particles back into the particle system
-    ps.SetTriggerParticles(ParticleSystemTriggerEventType.Enter, enter);
-    ps.SetTriggerParticles(ParticleSystemTriggerEventType.Exit, exit);
-  }
+    void OnParticleTrigger() 
+    {
+        // get the particles which matched the trigger conditions this frame
+        ps.GetTriggerParticles(ParticleSystemTriggerEventType.Inside, inside);
+        int numEnter = ps.GetTriggerParticles(ParticleSystemTriggerEventType.Enter, enter);
+        int numExit = ps.GetTriggerParticles(ParticleSystemTriggerEventType.Exit, exit);
+
+        //Debug.Log("trigger beg" + numEnter);
+
+        // iterate through the particles which entered the trigger and make them red
+        for (int i = 0; i < numEnter; i++) {
+          ParticleSystem.Particle p = enter[i];
+          p.startColor = new Color32(255, 0, 0, 255);
+
+          enter[i] = p;
+        }
+
+        // iterate through the particles which exited the trigger and make them green
+        for (int i = 0; i < numExit; i++) {
+          ParticleSystem.Particle p = exit[i];
+          p.startColor = new Color32(255, 255, 255, 255);
+          exit[i] = p;
+        }
+
+        // re-assign the modified particles back into the particle system
+        ps.SetTriggerParticles(ParticleSystemTriggerEventType.Enter, enter);
+        ps.SetTriggerParticles(ParticleSystemTriggerEventType.Exit, exit);
+
+        //Debug.Log("trigger " + enter.Count);
+    }
+
+
+    private void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            if(inside.Count > 0)
+            {
+                var go = new GameObject();
+                go.transform.SetParent(transform, false);
+                go.transform.localPosition = inside[0].position;
+                tmp_line = go.AddComponent<LineRenderer>();
+                tmp_line.material = line_material;
+                tmp_line.startWidth = tmp_line.endWidth = 2.0f;
+                tmp_line.useWorldSpace = false;
+                tmp_line.positionCount = 2;
+                tmp_line.SetPosition(0, Vector3.zero);
+            }
+        }
+
+        if(Input.GetMouseButtonUp(0))
+        {
+            if (tmp_line && inside.Count > 0)
+            {
+                tmp_line.SetPosition(1, tmp_line.transform.InverseTransformPoint(transform.TransformPoint(inside[0].position)));
+
+                constellation.lines.Add(tmp_line);
+                tmp_line = null;
+            }
+             
+            
+            if(tmp_line)
+            {
+                Destroy(tmp_line.gameObject);
+                tmp_line = null;
+            }
+        }
+
+        if (tmp_line)
+        {
+            var d = (transform.TransformPoint(tmp_line.GetPosition(0)) - Camera.main.transform.position).magnitude;
+            var mouse_pos = Input.mousePosition;
+            var ray = Camera.main.ScreenPointToRay(mouse_pos);
+
+            var pos = ray.origin + ray.direction * d;
+
+            tmp_line.SetPosition(1, tmp_line.transform.InverseTransformPoint(pos));
+        }
+    }
 }
--- master/Assets/Scripts/Gameplay/StarpickingRod.cs
+++ master/Assets/Scripts/Gameplay/StarpickingRod.cs
@@ -3,15 +3,50 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class StarpickingRod : MonoBehaviour {
+
+    public CameraControl m_camera_control;
+
+    bool m_enable_constellation_canvas = false;
+
   // Start is called before the first frame update
-  void Start() {}
+  void Start() 
+  {
+        m_enable_constellation_canvas = false;
+  }
 
   // Update is called once per frame
-  void Update() {
-    var mouse_pos = Input.mousePosition;
-    var ray = Camera.main.ScreenPointToRay(mouse_pos);
+  void Update() 
+  {
 
-    transform.position = ray.origin;
-    transform.rotation = Quaternion.LookRotation(ray.direction);
+        if(m_enable_constellation_canvas)
+        {
+            // disable main camera control
+            m_camera_control.enabled = false;
+
+            // enable picking rod
+            var rod_collider = GetComponent<Collider>();
+            rod_collider.enabled = true;
+
+
+            var mouse_pos = Input.mousePosition;
+            var ray = Camera.main.ScreenPointToRay(mouse_pos);
+
+            transform.position = ray.origin;
+            transform.rotation = Quaternion.LookRotation(ray.direction);
+        }
+        else
+        {
+            // enable main cam
+            m_camera_control.enabled = true;
+
+            // disbale rod
+            var rod_collider = GetComponent<Collider>();
+            rod_collider.enabled = false;
+        }
+  }
+
+  public void EnableConstellationMode() 
+  {
+        m_enable_constellation_canvas = !m_enable_constellation_canvas;
   }
 }
