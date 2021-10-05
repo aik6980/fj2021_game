bd31cd4d33423a512baef471b7ec093e9c029918
aik6980 <aik6980>
2021-10-06 00:06:50 +0100

    save load constellation

--- master/Assets/Scripts/Gameplay/Constellation.cs
+++ master/Assets/Scripts/Gameplay/Constellation.cs
@@ -1,11 +1,22 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
+[Serializable]
+public struct ConstellationData
+{
+    public List<Vector3S> go_position;
+    public List<Vector3S> beg_pos;
+    public List<Vector3S> end_pos;
+    public string name;
+}
 
 public class Constellation
 {
     public List<LineRenderer> lines;
     public string name;
+
     public Constellation()
     {
         lines = new List<LineRenderer>();
@@ -28,5 +39,49 @@ public class Constellation
         }
 
         return bounds;
+    }
+
+
+    public static ConstellationData convert(Constellation src)
+    {
+        var dest = new ConstellationData();
+        dest.name = src.name;
+        dest.go_position = new List<Vector3S>();
+        dest.beg_pos = new List<Vector3S>();
+        dest.end_pos = new List<Vector3S>();
+
+        foreach(var line in src.lines)
+        {
+            dest.go_position.Add(line.gameObject.transform.position);
+            dest.beg_pos.Add(line.GetPosition(0));
+            dest.end_pos.Add(line.GetPosition(1));
+        }
+
+        return dest;
+    }
+
+    public static Constellation convert(ConstellationData src)
+    {
+        var dest = new Constellation();
+        dest.name = src.name;
+        dest.lines = new List<LineRenderer>();
+
+        for(var i=0; i< src.beg_pos.Count; ++i)
+        {
+            var go = new GameObject();
+            go.transform.SetParent(ConstellationMgr.Instance.transform.parent, false);
+            go.transform.position = src.go_position[i];
+
+            var line = go.AddComponent<LineRenderer>();
+            line.useWorldSpace = false;
+            line.material = ConstellationMgr.Instance.line_material;
+            line.startWidth = line.endWidth = ConstellationMgr.Instance.line_width;
+            line.SetPosition(0, src.beg_pos[i]);
+            line.SetPosition(1, src.end_pos[i]);
+
+            dest.lines.Add(line);
+        }
+
+        return dest;
     }
 }
--- master/Assets/Scripts/Gameplay/ConstellationMgr.cs
+++ master/Assets/Scripts/Gameplay/ConstellationMgr.cs
@@ -4,9 +4,16 @@ using UnityEngine;
 using System.Linq;
 using UnityEngine.UI;
 
+
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization;
+
 public class ConstellationMgr : MonoSingleton<ConstellationMgr>
 {
     List<Constellation> m_constellation_list;
+    List<GameObject> m_constellation_name_list;
+
     bool m_enable_constellation_canvas;
 
     Constellation m_curr_constellation;
@@ -18,23 +25,27 @@ public class ConstellationMgr : MonoSingleton<ConstellationMgr>
     public Button activate_button;
     public float fade_time = 0.4f;
     private float _fade_velocity;
-	public Button exit_button;
-	public Button save_button;
-	public GameObject canvas_panel;
+    public Button exit_button;
+    public Button save_button;
+    public GameObject canvas_panel;
     public TMPro.TMP_InputField comp_name_inputfield;
+
+    public float line_width = 2.0f;
 
 
     Dictionary<Starpicking, List<ParticleSystem.Particle>> star_pickers = new Dictionary<Starpicking, List<ParticleSystem.Particle>>();
 
     bool process_undo = false;
 
-	public float pitchThresholdCosine = 0.6f;
+    public float pitchThresholdCosine = 0.6f;
 
 
-	// Start is called before the first frame update
-	void Start()
+    // Start is called before the first frame update
+    void Start()
     {
         m_constellation_list = new List<Constellation>();
+        m_constellation_name_list = new List<GameObject>();
+
         m_enable_constellation_canvas = false;
 
         canvas_panel.SetActive(false);
@@ -44,21 +55,39 @@ public class ConstellationMgr : MonoSingleton<ConstellationMgr>
         activate_button.image.color = color;
     }
 
-	public void Exit()
-	{
-		enable_canvas_mode(false);
-	}
-
-	public void Save()
-	{
-		enable_canvas_mode(false);
-	}
-
-	// Update is called once per frame
-	void Update()
+    public void Exit()
     {
-		if (!is_canvas_mode_enabled())
-		{
+        enable_canvas_mode(false);
+    }
+
+    public void Save()
+    {
+        enable_canvas_mode(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /// test save/load
+        if(Input.GetKeyDown(KeyCode.G))
+        {
+            save_constellation_data();
+        }
+
+        if(Input.GetKeyDown(KeyCode.H))
+        {
+            reset_constellation_data();
+            load_constellation_data();
+        }
+
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            reset_constellation_data();
+        }
+        ///
+
+        if (!is_canvas_mode_enabled())
+        {
             bool activation_button_enabled = Camera.main.transform.forward.y > pitchThresholdCosine;
             float opacity_target = (activate_button.enabled ? 1f : 0f);
 
@@ -66,15 +95,15 @@ public class ConstellationMgr : MonoSingleton<ConstellationMgr>
             var color = activate_button.image.color;
             color.a = Mathf.SmoothDamp(color.a, opacity_target, ref _fade_velocity, fade_time);
             activate_button.image.color = color;
-        } 
+        }
         else
-		{
-			bool hasName = !string.IsNullOrEmpty(comp_name_inputfield.text);
-			save_button.gameObject.SetActive(hasName && m_curr_constellation.lines.Count > 0);
-			exit_button.gameObject.SetActive(!hasName || m_curr_constellation.lines.Count == 0);
-		}
+        {
+            bool hasName = !string.IsNullOrEmpty(comp_name_inputfield.text);
+            save_button.gameObject.SetActive(hasName && m_curr_constellation.lines.Count > 0);
+            exit_button.gameObject.SetActive(!hasName || m_curr_constellation.lines.Count == 0);
+        }
 
-		if (Input.GetMouseButtonDown(1) || process_undo)
+        if (Input.GetMouseButtonDown(1) || process_undo)
         {
             process_undo = false;
 
@@ -99,7 +128,7 @@ public class ConstellationMgr : MonoSingleton<ConstellationMgr>
             var star_picking = kvp.Key;
             var inside = kvp.Value;
 
-            if(inside != null)
+            if (inside != null)
             {
                 if (inside.Count > 0)
                 {
@@ -108,7 +137,7 @@ public class ConstellationMgr : MonoSingleton<ConstellationMgr>
                     go.transform.localPosition = inside[0].position;
                     m_tmp_line = go.AddComponent<LineRenderer>();
                     m_tmp_line.material = line_material;
-                    m_tmp_line.startWidth = m_tmp_line.endWidth = 2.0f;
+                    m_tmp_line.startWidth = m_tmp_line.endWidth = line_width;
                     m_tmp_line.useWorldSpace = false;
                     m_tmp_line.positionCount = 2;
                     m_tmp_line.SetPosition(0, Vector3.zero);
@@ -156,9 +185,9 @@ public class ConstellationMgr : MonoSingleton<ConstellationMgr>
 
     public KeyValuePair<Starpicking, List<ParticleSystem.Particle>> get_first_inside_list()
     {
-        foreach(var kvp in star_pickers)
+        foreach (var kvp in star_pickers)
         {
-            if(kvp.Value.Count > 0)
+            if (kvp.Value.Count > 0)
             {
                 return kvp;
             }
@@ -176,7 +205,7 @@ public class ConstellationMgr : MonoSingleton<ConstellationMgr>
     {
         m_enable_constellation_canvas = val;
 
-        if(m_enable_constellation_canvas == true)
+        if (m_enable_constellation_canvas == true)
         {
             m_curr_constellation = new Constellation();
 
@@ -194,13 +223,13 @@ public class ConstellationMgr : MonoSingleton<ConstellationMgr>
 
                 // clone then add
                 var new_constellation = new Constellation();
+                new_constellation.name = name;
                 foreach (var line in m_curr_constellation.lines)
                 {
-                    var go_clone = Instantiate(line.gameObject, line.gameObject.transform.parent);
+                    // var go_clone = Instantiate(line.gameObject, line.gameObject.transform.parent);
+                    var go_clone = Instantiate(line.gameObject, transform.parent);
                     new_constellation.lines.Add(go_clone.GetComponent<LineRenderer>());
                 }
-
-                m_constellation_list.Add(new_constellation);
 
                 // create a Constellation GO with name tag
                 var go = Instantiate(constellation_prefab);
@@ -209,12 +238,14 @@ public class ConstellationMgr : MonoSingleton<ConstellationMgr>
 
                 go.transform.SetParent(transform, false);
                 go.transform.position = bound.center;
-                go.GetComponentInChildren<TMPro.TMP_Text>().text = name;
-                
+                go.GetComponentInChildren<TMPro.TMP_Text>().text = new_constellation.name;
+
+                m_constellation_list.Add(new_constellation);
+                m_constellation_name_list.Add(go);
             }
 
             // destroy the temp one
-            foreach(var line in m_curr_constellation.lines)
+            foreach (var line in m_curr_constellation.lines)
             {
                 Destroy(line.gameObject);
             }
@@ -223,6 +254,9 @@ public class ConstellationMgr : MonoSingleton<ConstellationMgr>
 
             canvas_panel.SetActive(false);
             activate_button.gameObject.SetActive(true);
+
+            // save canvas on every new drawing
+            save_constellation_data();
         }
     }
 
@@ -233,7 +267,7 @@ public class ConstellationMgr : MonoSingleton<ConstellationMgr>
     {
         string myString = "";
         int charAmount = Random.Range(minCharAmount, maxCharAmount); //set those to the minimum and maximum length of your string
-        for(int i=0; i<charAmount; i++)
+        for (int i = 0; i < charAmount; i++)
         {
             myString += glyphs[Random.Range(0, glyphs.Length)];
         }
@@ -257,4 +291,87 @@ public class ConstellationMgr : MonoSingleton<ConstellationMgr>
     {
         process_undo = true;
     }
+
+    const string file_name = "/my_constellation.dat";
+    public void save_constellation_data()
+    {
+        FileStream fs = new FileStream(Application.persistentDataPath + file_name, FileMode.Create);
+
+        BinaryFormatter bf = new BinaryFormatter();
+
+        //var test_data = new ConstellationData();
+        //test_data.name = "hello world";
+        //test_data.beg_pos = new List<Vector3S>();
+        //test_data.end_pos = new List<Vector3S>();
+        //
+        //test_data.beg_pos.Add(new Vector3(0.4f, 0.5f, 0.6f));
+        //test_data.end_pos.Add(new Vector3(3.4f, 4.5f, 5.6f));
+
+        var data = new List<ConstellationData>();
+        foreach(var obj in m_constellation_list)
+        {
+            var obj_data = Constellation.convert(obj);
+            data.Add(obj_data);
+        }
+
+
+        bf.Serialize(fs, data);
+
+        //Debug.Log(Application.persistentDataPath);
+
+        fs.Close();
+
+        Debug.Log("data saved");
+    }
+
+    public void load_constellation_data()
+    {
+        Debug.Log("data loading");
+        if (File.Exists(Application.persistentDataPath + file_name))
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.Open(Application.persistentDataPath + file_name, FileMode.Open);
+
+            var data = (List<ConstellationData>)bf.Deserialize(file);
+            file.Close();
+
+            foreach(var obj_data in data)
+            {
+                var obj = Constellation.convert(obj_data);
+
+                // create a Constellation GO with name tag
+                var go = Instantiate(constellation_prefab);
+                var bound = obj.create_bounds();
+                go.GetComponent<SphereCollider>().radius = bound.extents.magnitude;
+
+                go.transform.SetParent(transform, false);
+                go.transform.position = bound.center;
+                go.GetComponentInChildren<TMPro.TMP_Text>().text = obj.name;
+
+                m_constellation_list.Add(obj);
+                m_constellation_name_list.Add(go);
+            }
+
+            Debug.Log("data loaded");
+        }
+    }
+
+    public void reset_constellation_data()
+    {
+        foreach(var a in m_constellation_list)
+        {
+            foreach(var b in a.lines)
+            {
+                Destroy(b.gameObject);
+            }
+        }
+        m_constellation_list.Clear();
+
+        foreach(var a in m_constellation_name_list)
+        {
+            Destroy(a);
+        }
+        m_constellation_name_list.Clear();
+    }
+
 }
