9ab50b17fd9d9185120cd4263ddf0bfce318c762
Frictionpowered <denes@nagymathe.com>
2021-10-07 17:56:30 +0100

    applied settings to prefabs from scene

--- master/Assets/Scripts/RespawnMechanics.cs
+++ master/Assets/Scripts/RespawnMechanics.cs
@@ -4,8 +4,35 @@ using UnityEngine;
 
 public class RespawnMechanics : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+	public GameObject player;
+	public GameObject ship;
+
+	public Vector3 respawnLocation_Player;
+	public Vector3 respawnLocation_Ship;
+	public Quaternion respawnRotation_Player;
+	public Quaternion respawnRotation_Ship;
+
+	public float ageYearLengthInSeconds = 5;	//~8 minutes to reach 100 years
+	public float currentAge = 0;
+	public float maxAge = 100;
+
+	private void Awake()
+	{
+		if (player)
+		{
+			respawnLocation_Player = player.transform.position;
+			respawnRotation_Player = player.transform.rotation;
+		}
+		if (ship)
+		{
+			respawnLocation_Ship = ship.transform.position;
+			respawnRotation_Ship = ship.transform.rotation;
+		}
+	}
+
+
+	// Start is called before the first frame update
+	void Start()
     {
         
     }
@@ -13,6 +40,17 @@ public class RespawnMechanics : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+		currentAge += Time.deltaTime / ageYearLengthInSeconds;
+
+		//debug
+		if (Input.GetKeyDown(KeyCode.R) && Input.GetKey(KeyCode.LeftControl))
+		{
+			ExecuteRespawn();
+		}
     }
+
+	public void ExecuteRespawn()
+	{
+		currentAge = 0;
+	}
 }
