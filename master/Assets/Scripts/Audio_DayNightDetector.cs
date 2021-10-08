bb523da6479e421770b2f5e5bbe4c854f02e3ce7
Neil Doyle <neildoyle.92@gmail.com>
2021-10-08 03:11:37 +0100

    added day/night detection to emitters. Currently seems to be broken :(

--- master/Assets/Scripts/Audio_DayNightDetector.cs
+++ master/Assets/Scripts/Audio_DayNightDetector.cs
@@ -7,22 +7,32 @@ public class Audio_DayNightDetector : MonoBehaviour
     public GameObject DayLightCycle;
 
     private FMOD.Studio.EventInstance instance;
+    private FMOD.Studio.PARAMETER_ID day_nightParameterID;
     
     [FMODUnity.EventRef]
     public string fmodEvent;
+    
 
     // Start is called before the first frame update
     void Start()
     {
         instance = FMODUnity.RuntimeManager.CreateInstance(fmodEvent);
+
+        FMOD.Studio.EventDescription day_nightEventDescription;
+        instance.getDescription(out day_nightEventDescription);
+        FMOD.Studio.PARAMETER_DESCRIPTION day_nightParameterDescription;
+        day_nightEventDescription.getParameterDescriptionByName("day_night", out day_nightParameterDescription);
+        day_nightParameterID = day_nightParameterDescription.id;
+        
         instance.start();
+
+        FMODUnity.RuntimeManager.AttachInstanceToGameObject(instance, GetComponent<Transform>(), GetComponent<Rigidbody>());
     }
 
     // Update is called once per frame
     void Update()
     {
         var daylight_script = DayLightCycle.GetComponent<DayLight>();
-        Debug.Log(daylight_script.normalisedDayTime);
-        instance.setParameterByName("day_night", daylight_script.normalisedDayTime);
+        instance.setParameterByID(day_nightParameterID, daylight_script.normalisedDayTime);
     }
 }
