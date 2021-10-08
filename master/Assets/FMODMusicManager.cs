7331e15074bdcdf550abf9856457c553e21b380f
Neil Doyle <neildoyle.92@gmail.com>
2021-10-08 15:47:12 +0100

    added intro music

--- master/Assets/FMODMusicManager.cs
+++ master/Assets/FMODMusicManager.cs
@@ -7,8 +7,22 @@ public class FMODMusicManager : MonoBehaviour
 
     public CameraControl camera_control;
     public Vector3 velocity;
+    public GameObject otherObject;
 
-    private FMOD.Studio.EventInstance music_stargazing, music_sailing;
+    private FMOD.Studio.EventInstance music_stargazing, music_sailing, music_introstart, music_introend;
+
+    FMOD.Studio.PLAYBACK_STATE PlaybackState_IntroStart(FMOD.Studio.EventInstance music_introstart)
+        {
+            FMOD.Studio.PLAYBACK_STATE pS;
+            music_introstart.getPlaybackState(out pS);
+            return pS;        
+        }
+    FMOD.Studio.PLAYBACK_STATE PlaybackState_IntroEnd(FMOD.Studio.EventInstance music_introend)
+        {
+            FMOD.Studio.PLAYBACK_STATE pS;
+            music_introend.getPlaybackState(out pS);
+            return pS;        
+        }
     FMOD.Studio.PLAYBACK_STATE PlaybackState_Stargazing(FMOD.Studio.EventInstance music_stargazing)
         {
             FMOD.Studio.PLAYBACK_STATE pS;
@@ -25,12 +39,28 @@ public class FMODMusicManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+            music_introstart = FMODUnity.RuntimeManager.CreateInstance("event:/Music/music_introstart");
+            music_introstart.start();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (otherObject.activeSelf != true)
+        {
+            if (PlaybackState_IntroStart(music_introstart) == FMOD.Studio.PLAYBACK_STATE.PLAYING)
+            {
+                music_introstart.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+                music_introstart.release();
+            }
+            if (PlaybackState_IntroEnd(music_introend) != FMOD.Studio.PLAYBACK_STATE.PLAYING)
+            {
+                music_introend = FMODUnity.RuntimeManager.CreateInstance("event:/Music/music_introend");
+                music_introend.start();
+                music_introend.release();
+            }
+        }
+
         if(ConstellationMgr.Instance.is_canvas_mode_enabled())
         {
             if (PlaybackState_Stargazing(music_stargazing) != FMOD.Studio.PLAYBACK_STATE.PLAYING)
